dfc5c7ba366cf63a01fd6e589319d2185b434bbf
thecallmeBilalAshiq <f223309@cfd.nu.edu.pk>
2025-04-02 17:02:06 +0500

    Adding folders for Computer networks project

---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System.Windows.Forms;
+
+namespace ChatClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.sendFileButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chatBox
+            // 
+            this.chatBox.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chatBox.Location = new System.Drawing.Point(10, 68);
+            this.chatBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.chatBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatBox.Size = new System.Drawing.Size(538, 436);
+            this.chatBox.TabIndex = 0;
+            this.chatBox.Text = "                                           End-End Encrypted Chat";
+            this.chatBox.TextChanged += new System.EventHandler(this.chatBox_TextChanged);
+            // 
+            // messageBox
+            // 
+            this.messageBox.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageBox.Location = new System.Drawing.Point(18, 580);
+            this.messageBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.messageBox.Multiline = true;
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(414, 72);
+            this.messageBox.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.Location = new System.Drawing.Point(445, 580);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(112, 35);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // sendFileButton
+            // 
+            this.sendFileButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.sendFileButton.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.P2P_chat_background__with_attractive_colors__no_text_____and_purple__pink__blue____attractive_colrors_like_cyberpunk;
+            this.sendFileButton.Font = new System.Drawing.Font("Verdana", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendFileButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.sendFileButton.Location = new System.Drawing.Point(444, 625);
+            this.sendFileButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sendFileButton.Name = "sendFileButton";
+            this.sendFileButton.Size = new System.Drawing.Size(112, 35);
+            this.sendFileButton.TabIndex = 3;
+            this.sendFileButton.Text = "Send File";
+            this.sendFileButton.UseVisualStyleBackColor = false;
+            this.sendFileButton.Click += new System.EventHandler(this.sendFileButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.BackColor = System.Drawing.Color.Silver;
+            this.statusLabel.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(13, 669);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(543, 31);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "Disconnected";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.P2P_chat_background__with_attractive_colors__no_text_____and_purple__pink__blue____attractive_colrors_like_cyberpunk;
+            this.button1.Font = new System.Drawing.Font("Constantia", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button1.Location = new System.Drawing.Point(11, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(538, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "P2P Application Chat";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(559, 706);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.sendFileButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.chatBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Chat Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button sendFileButton;
+        private System.Windows.Forms.Label statusLabel;
+        private Button button1;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class MainForm : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread receiveThread;
+        private volatile bool isClosing = false;
+        private string username;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeEmojiButtons();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ConnectToServer();
+        }
+
+
+        //  ========================== >>> Connect to Server <<< ===========================
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient("localhost", 8080);
+                stream = client.GetStream();
+                username = Prompt.ShowDialog("Enter your username: ", "Login");
+                if (string.IsNullOrEmpty(username))
+                {
+                    MessageBox.Show("Username cannot be empty. Exiting.");
+                    Application.Exit();
+                    return;
+                }
+
+                UpdateStatus(username + " connected");
+                receiveThread = new Thread(ReceiveMessages);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to server: " + ex.Message);
+                Application.Exit();
+            }
+        }
+
+        //  ========================== >>> Recieve messgages <<< ===========================
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[4096];
+            while (!isClosing)
+            {
+                try
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        if (message.StartsWith("START_FILE_TRANSFER:"))
+                        {
+                            string filename = message.Split(':')[1];
+                            ReceiveFile(filename);
+                        }
+                        else
+                        {
+                            UpdateChatBox(message);
+                        }
+                    }
+                }
+                catch
+                {
+                    UpdateStatus("Disconnected from server.");
+                    break;
+                }
+            }
+        }
+
+
+        //  ========================== >>> Recieve File  <<< ===========================
+        private void ReceiveFile(string filename)
+        {
+            string downloadsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            Directory.CreateDirectory(downloadsFolder);
+
+            string filePath = Path.Combine(downloadsFolder, filename);
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                byte[] buffer = new byte[4096];
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead <= 0) break;
+
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    if (data.Contains("END_FILE_TRANSFER"))
+                    {
+                        int endIndex = data.IndexOf("END_FILE_TRANSFER");
+                        fs.Write(buffer, 0, endIndex);
+                        break;
+                    }
+
+                    fs.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            UpdateChatBox($"File saved to Downloads: {filePath}");
+        }
+
+
+        //  ========================== >>>  <<< ===========================
+
+        private void UpdateChatBox(string message)
+        {
+            if (chatBox.InvokeRequired)
+            {
+                Invoke(new Action(() => chatBox.AppendText(message + Environment.NewLine)));
+            }
+            else
+            {
+                chatBox.AppendText(message + Environment.NewLine);
+            }
+        }
+
+
+        //  ========================== >>>  <<< ===========================
+        private void UpdateStatus(string message)
+        {
+            if (this.IsDisposed)
+                return;
+
+            if (statusLabel.InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    if (!this.IsDisposed)
+                        statusLabel.Text = message;
+                }));
+            }
+            else
+            {
+                if (!this.IsDisposed)
+                    statusLabel.Text = message;
+            }
+        }
+
+        //  ========================== >>> send button  <<< ===========================
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string message = messageBox.Text.Trim();
+            if (!string.IsNullOrEmpty(message) && stream != null)
+            {
+                try
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes($"SEND_MESSAGE:{username}: {message}");
+                    stream.Write(buffer, 0, buffer.Length);
+                    UpdateChatBox($"You::     {message}");
+                    messageBox.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to send message: {ex.Message}");
+                }
+            }
+        }
+
+        //  ========================== >>> send file button <<< ===========================
+        private void sendFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = fileDialog.FileName;
+                string fileName = Path.GetFileName(filePath);
+                byte[] fileData = File.ReadAllBytes(filePath);
+
+                string fileHeader = $"UPLOAD_FILE:{fileName}";
+                byte[] headerBuffer = Encoding.UTF8.GetBytes(fileHeader);
+                stream.Write(headerBuffer, 0, headerBuffer.Length);
+
+                stream.Write(fileData, 0, fileData.Length);
+
+                byte[] endBuffer = Encoding.UTF8.GetBytes("END_FILE_UPLOAD");
+                stream.Write(endBuffer, 0, endBuffer.Length);
+
+                UpdateChatBox($"You sent a file: {fileName}");
+            }
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isClosing = true;
+            try
+            {
+                if (receiveThread != null && receiveThread.IsAlive)
+                {
+                    receiveThread.Join(500);
+                }
+
+                stream?.Close();
+                client?.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during cleanup: {ex.Message}");
+            }
+        }
+
+
+
+        //  ================================================= >>>  Emojis <<< ===========================================================
+
+
+        private void InitializeEmojiButtons()
+        {
+            string[] emojis = { "😀", "😂", "😍", "👍", "❤️" };
+            int xPos = 20;
+            int yPos = 340;
+
+            foreach (string emoji in emojis)
+            {
+                Button emojiButton = new Button
+                {
+                    Text = emoji,
+                    Location = new System.Drawing.Point(xPos, yPos),
+                    Size = new System.Drawing.Size(35, 35),
+                    FlatStyle = FlatStyle.Flat,
+                    Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                    BackColor = System.Drawing.Color.SpringGreen  // Set background color
+                };
+
+                emojiButton.Click += (sender, e) =>
+                {
+                    SendEmoji(emoji);
+                };
+
+                Controls.Add(emojiButton);
+                xPos += 50;
+            }
+        }
+
+
+        //  ========================== >>> Send Emoji  <<< ===========================
+        private void SendEmoji(string emoji)
+        {
+            if (stream != null)
+            {
+                try
+                {
+                    string message = $"SEND_EMOJI:{username}: {emoji}";
+                    byte[] buffer = Encoding.UTF8.GetBytes(message);
+                    stream.Write(buffer, 0, buffer.Length);
+                    UpdateChatBox($"You:     {emoji}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to send emoji: {ex.Message}");
+                }
+            }
+        }
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form
+                {
+                    Width = 400,
+                    Height = 150,
+                    Text = caption
+                };
+
+                Label textLabel = new Label() { Left = 10, Top = 20, Text = text };
+                TextBox inputBox = new TextBox() { Left = 10, Top = 50, Width = 350 };
+                Button confirmButton = new Button() { Text = "OK", Left = 280, Width = 80, Top = 80 };
+
+                confirmButton.Click += (sender, e) =>
+                {
+                    prompt.DialogResult = DialogResult.OK;
+                    prompt.Close();
+                };
+
+                prompt.Controls.Add(textLabel);
+                prompt.Controls.Add(inputBox);
+                prompt.Controls.Add(confirmButton);
+                prompt.AcceptButton = confirmButton;
+
+                return prompt.ShowDialog() == DialogResult.OK ? inputBox.Text : string.Empty;
+            }
+        }
+
+        private void chatBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
